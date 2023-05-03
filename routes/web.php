c1fec9f31bb92abdbf7378a43f2573d62ef7443e
Tarik Benali amar <tarikbenaliamar@gmail.com>
2023-05-03 18:18:27 +0100

    auth: login/register

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\UserAuthController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +17,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(UserAuthController::class)->group(function(){
+
+    Route::get('login', 'index')->name('login');
+
+    Route::get('registration', 'registration')->name('registration');
+
+    Route::get('logout', 'logout')->name('logout');
+
+    Route::post('validate_registration', 'validate_registration')->name('sample.validate_registration');
+
+    Route::post('validate_login', 'validate_login')->name('sample.validate_login');
+
+    Route::get('dashboard', 'dashboard')->name('dashboard');
 });
